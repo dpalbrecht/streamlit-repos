51ce20b4310521f9d5ea015f240e403ee1a64e35
David Albrecht <davidpabloalbrecht@gmail.com>
2023-04-15 21:04:19 -0700

    add base files

---
+++ app.py
@@ -0,0 +1,2 @@
+import streamlit as st
+from css import load_css; load_css()
---
+++ css.py
@@ -0,0 +1,45 @@
+import streamlit as st
+
+
+
+def load_css():
+    st.set_page_config(layout="wide")
+    margins_css = """
+    <style>
+        .main > div {
+            padding-top: 2rem;
+        }
+        div.h1 {
+            font-size: 60px;
+            font-weight: bold;
+            text-align: center;
+        }
+        div.h4 {
+            font-size: 20px;
+            text-align: center;
+            padding-bottom: 50px;
+        }
+        div.stButton>button {
+                border: 1px solid transparent;
+                font-size: 30px;
+                border-radius: 0.25rem;
+                color: #fff;
+                background-color: #007bff;
+                border-color: #007bff;
+                text-align: center;
+            }
+        div.stButton>button:hover {
+            border: 1px solid transparent;
+            font-size: 30px;
+            border-radius: 0.25rem;
+            color: #fff;
+            background-color: #383;
+            border-color: #007bff;
+        }
+        table, th, td {
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+    </style>
+    """
+    st.markdown(margins_css, unsafe_allow_html=True)
